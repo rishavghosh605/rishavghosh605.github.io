f58d9011083e0004c579e9e21b86176eff687df1
rishavghosh605 <rishavghosh605@gmail.com>
2020-10-01 09:57:18 +0530

    Header properties changed, menu cross changed

--- js/main.js
+++ js/main.js
@@ -2,10 +2,14 @@ function menuToCross(x) {
   x.classList.toggle("change");
   overlayMenu = document.getElementById("overlay-content");
   overlayMenu.classList.toggle("on");
+  logoIcon = document.getElementById("logo-icon");
+  logoIcon.classList.toggle("change");
+  navbar = document.getElementById("nav");
+  navbar.classList.toggle("change");
   menuText = document.getElementById("menu-text");
   body = document.getElementsByTagName("body");
   bodyTag = body[0];
-  bodyTag.classList.toggle("fix");//Fix so that there is no overflow when menu appears
+  bodyTag.classList.toggle ("fix");//Fix so that there is no overflow when menu appears
 }
 
 window.addEventListener("scroll", function (event) {
