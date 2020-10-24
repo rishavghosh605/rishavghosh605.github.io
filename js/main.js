6d4e9bf376b131d7a15504f57c4bfb6f4f70df50
rishavghosh605 <rishavghosh605@gmail.com>
2020-10-24 22:07:32 +0530

    Newsletter added and contact form ui added

--- js/main.js
+++ js/main.js
@@ -14,28 +14,61 @@ function menuToCross(x) {
 
 window.addEventListener("scroll", function (event) {
   var scroll_y = this.scrollY;
-  var scroll_x = this.scrollX;
   var header = document.getElementById("top-content");
   var navbar = document.getElementById("nav");
-  var scroller = document.getElementsByClassName("scroll-on");
-  var scrollUp = document.getElementById("scroll-up-icon");
   var profileImage = document.getElementById("head-image");
 
   var height =
     header.getBoundingClientRect().height +
-    navbar.getBoundingClientRect().height;
-  var displayScrollUp = scroll_y >= height + 2 ? true : false;
+    navbar.getBoundingClientRect().height
   
   var profileOpacity = (height-scroll_y)/height;
 
   if(profileOpacity>=0){
     profileImage.style.opacity = profileOpacity;
   }
-  if (displayScrollUp && scroller.length == 0) {
-    scrollUp.classList.toggle("scroll-on");
-    profileImage.classList.toggle("off");
-  } else if (!displayScrollUp && scroller.length != 0) {
-    scrollUp.classList.toggle("scroll-on");
-    profileImage.classList.toggle("off");
-  }
 });
+
+//Intersection observer for scroll-up icon animation
+const scrollDown = document.querySelector('.scroll-to-icon');
+const scrollUp = document.getElementById("scroll-up-icon");
+
+observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio == 0) {
+      scrollUp.classList.add("scroll-on");
+    } else {
+        scrollUp.classList.remove("scroll-on");
+    }
+  });
+});
+observer.observe(scrollDown);
+
+
+// //Using Intersection Observer for fadeIn animation
+const myElements = document.querySelectorAll('.lazyload');
+
+observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.intersectionRatio > 0) {
+      console.log('in the view');
+      entry.target.classList.add('fadeIn');
+      observer.unobserve(entry.target);
+    } else {
+      console.log('out of view');
+      // entry.target.classList.remove('fancy');
+    }
+  });
+});
+
+myElements.forEach(element => {
+  observer.observe(element);
+});
+
+// $(document).ready(function(){
+//   $(document).ready(function(){
+//     $("textarea").keypress(function(){
+//       $("span.input-bottom-line").text(i += 1);
+//     });
+//   });
+// });
