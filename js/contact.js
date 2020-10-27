ff56c7a240f91bf3573144adce41a3946b8e99a0
rishavghosh605 <rishavghosh605@gmail.com>
2020-10-27 21:57:30 +0530

    Thankyou page modified

--- js/contact.js
+++ js/contact.js
@@ -18,17 +18,11 @@ myElements.forEach(element => {
 function onSubmit(token) {
   document.getElementById("form-contact").submit();
 }
-// i=0;
-// $(document).ready(function(){
-//     $("textarea").keypress(function(event){
-//       var keycode = (event.keyCode ? event.keyCode : event.which);
-// 	if(keycode == '13'){
-//     console.log("ddf");
-//     $(this).prop("scrollHeight") / parseInt($(this).css("line-height"), 10) > y.textareaLines ? $(this).css("overflow-y", "scroll") : ($(this).css("overflow-y", "hidden"), $(this).css("height", "auto"), $(this).css("height", $(this).prop("scrollHeight") + 1 + "px"))
-// 	}
-//       $("span.input-bottom-line").text(i += 1);
 
-//     });
-//   });   
 
-  // 
+i=0;
+$(document).ready(function(){
+  autosize($('textarea'));
+    });   
+
+  
